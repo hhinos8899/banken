1060b5d2ff6fbc2b63c389bd8bfbc10e8e0b35d9
hhinos8899 <jifengcheng483@gmail.com>
2025-12-28 21:55:13 +0800

    Initialize Supabase client in supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "https://nljvpnmcnzeibosrtnlp.supabase.co;
+const SUPABASE_ANON_KEY = "sb_publishable_zvkqWardciRVVm4PpzU6Lg_FTZbeqJ3";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
